180629d1833865ecaba3c53ea91ee0720fb05ccd
Mohamed Shil <maxamed-14@outlook.dk>
2024-10-18 23:58:33 +0200

    add storage account

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name = "createdbyterraform"
 }
+
+resource "azurerm_storage_account" "sa" {
+  location = "northeurope"
+  name = "acccreatedbyterraform"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  resource_group_name = azurerm_resource_group.rg.name
+}
