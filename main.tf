6f61756202c26e46ee4d794a35b835adb7c86825
Mohamed Shil <maxamed-14@outlook.dk>
2024-10-20 14:05:56 +0200

    add mssql_server

--- main.tf
+++ main.tf
@@ -33,3 +33,12 @@ resource "azurerm_monitor_metric_alert" "metric" {
     aggregation      = "Total"
   }
 }
+
+resource "azurerm_mssql_server" "ms_server" {
+  name = "servercreatedbyterraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = "northeurope"
+  version = "2.0"
+  administrator_login = "adminloginmo"
+  administrator_login_password = "adminloginmo123"
+}
